257cdbb6a175464695b6a56da3e50d179833ab1e
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-06-23 20:22:07 +0100

    This is initial commit of printing information on the screen using JavaScript

---
+++ README.md
@@ -0,0 +1,3 @@
+#### This is to print my name, height and country on the screen
+
+#### This is the link to the github published page: 
---
+++ index.js
@@ -0,0 +1,5 @@
+// This is to print my name, height and Country on the screen
+
+alert(`My name is Chibuike Chijioke.
+I am 6ft tall.
+I am a citizen of Nigeria.`)
